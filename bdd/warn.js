3f979be3147fca4119f03a2975eb0af6aea6ea62
ShadowSMR <spartakinyui78@gmail.com>
2025-06-12 18:16:59 +0300

    Update warn.js

--- bdd/warn.js
+++ bdd/warn.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Path to the JSON file storing warning user data
-const filePath = path.join(__dirname, '../popkid/warn_users.json');
+const filePath = path.join(__dirname, '../SHADOW-SMR-MD/warn_users.json');
 
 // Load data from the JSON file
 function loadWarnData() {
