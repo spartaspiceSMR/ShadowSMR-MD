eb705c37f011d5bfa3787ee7c51c2723176dec76
ShadowSMR <spartakinyui78@gmail.com>
2025-06-12 18:11:12 +0300

    Update hentai.js

--- bdd/hentai.js
+++ bdd/hentai.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Path to the JSON file storing hentai data
-const filePath = path.join(__dirname, '../popkid/hentai.json');
+const filePath = path.join(__dirname, '../SHADOW-SMR-MD/hentai.json');
 
 // Load data from JSON file
 function loadHentaiData() {
