868f45b3c271b3835d987fc49df3d7cfbf5ab7dc
ShadowSMR <spartakinyui78@gmail.com>
2025-06-12 18:13:25 +0300

    Update mention.js

--- bdd/mention.js
+++ bdd/mention.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Path to the JSON file storing mention data
-const filePath = path.join(__dirname, '../popkid/mention.json');
+const filePath = path.join(__dirname, '../SHADOW-SMR-MD/mention.json');
 
 // Load data from the JSON file
 function loadMentionData() {
