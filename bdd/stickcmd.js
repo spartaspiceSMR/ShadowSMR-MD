68d79c24e9fcb2e9c57eeeeafd64740c5ec39cdc
ShadowSMR <spartakinyui78@gmail.com>
2025-06-12 18:15:33 +0300

    Update stickcmd.js

--- bdd/stickcmd.js
+++ bdd/stickcmd.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Path to the JSON file storing stickcmd data
-const filePath = path.join(__dirname, '../popkid/stickcmd.json');
+const filePath = path.join(__dirname, '../SHADOW-SMR-MD/stickcmd.json');
 
 // Load data from the JSON file
 function loadStickcmdData() {
